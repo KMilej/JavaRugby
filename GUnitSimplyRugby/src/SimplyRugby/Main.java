f2d54f0c6c54a03889c23445e4b068812290af20
KM <kmilejside@gmail.com>
2025-04-29 18:35:14 +0100

    First Commit

---
+++ GUnitSimplyRugby/src/SimplyRugby/Main.java
@@ -0,0 +1,11 @@
+package SimplyRugby;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Program SimplyRugbySystem = new Program();
+		SimplyRugbySystem.hello();
+	}
+
+}
---
+++ GUnitSimplyRugby/src/SimplyRugby/Program.java
@@ -0,0 +1,10 @@
+package SimplyRugby;
+
+public class Program {
+	
+	
+	public void hello() {
+		System.out.println("HEllo");
+	}
+	
+}
